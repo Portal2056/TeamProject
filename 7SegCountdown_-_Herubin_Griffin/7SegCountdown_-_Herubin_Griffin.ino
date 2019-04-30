1271aaacc402e58c4530fa56181bae0324c090ef
Corey Herubin <02corey.herubin@gulllakecs.org>
2019-04-30 12:48:47 -0400

    Initial Commit

---
+++ 7SegCountdown_-_Herubin_Griffin/7SegCountdown_-_Herubin_Griffin.ino
@@ -0,0 +1,21 @@
+/*
+
+Corey Herubin
+Korbin Griffin
+
+4.30.19
+
+This project is going to use a button and a 7-segment 
+display to countdown from 9 to 0
+
+*/
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
